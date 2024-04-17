7b0fa1132d79d228fa2c70ba6b666931d5c8fc19
ixiMOREIRA <rensouzajunior@gmail.com>
2024-04-17 16:11:22 -0300

    inicial

---
+++ sscript.js
@@ -0,0 +1,135 @@
+document.getElementById('a1').addEventListener('click', function(e) {
+    e.preventDefault();
+    let destino = document.getElementById('img');
+    destino.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('a2').addEventListener('click', function(e) {
+    e.preventDefault();
+    let destino = document.getElementById('nome');
+    destino.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('a3').addEventListener('click', function(e) {
+    e.preventDefault();
+    let destino = document.getElementById('ttk1');
+    destino.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('a4').addEventListener('click', function(e) {
+    e.preventDefault();
+    let destino = document.getElementById('projetos');
+    destino.scrollIntoView({ behavior: 'smooth' });
+});
+
+function encima1() {
+    const txt = document.getElementById('textoreact')
+    const nada = document.getElementById('tudo')
+    txt.style.display = 'block';
+    nada.style.display = 'none';
+}
+
+function fora1() {
+    const txt = document.getElementById('textoreact')
+    const nada = document.getElementById('tudo')
+    txt.style.display = 'none';
+    nada.style.display = 'block';
+}
+
+function encima2() {
+    const txt = document.getElementById('textoboots')
+    const nada = document.getElementById('tudo')
+    txt.style.display = 'block';
+    nada.style.display = 'none';
+}
+
+function fora2() {
+    const txt = document.getElementById('textoboots')
+    const nada = document.getElementById('tudo')
+    txt.style.display = 'none';
+    nada.style.display = 'block';
+}
+
+function encima3() {
+    const txt = document.getElementById('textotail')
+    const nada = document.getElementById('tudo')
+    txt.style.display = 'block';
+    nada.style.display = 'none';
+}
+
+function fora3() {
+    const txt = document.getElementById('textotail')
+    const nada = document.getElementById('tudo')
+    txt.style.display = 'none';
+    nada.style.display = 'block';
+}
+
+function encima4() {
+    const txt = document.getElementById('textogit')
+    const nada = document.getElementById('tudo')
+    txt.style.display = 'block';
+    nada.style.display = 'none';
+}
+
+function fora4() {
+    const txt = document.getElementById('textogit')
+    const nada = document.getElementById('tudo')
+    txt.style.display = 'none';
+    nada.style.display = 'block';
+}
+
+function encima5() {
+    const txt = document.getElementById('textogithub')
+    const nada = document.getElementById('tudo')
+    txt.style.display = 'block';
+    nada.style.display = 'none';
+}
+
+function fora5() {
+    const txt = document.getElementById('textogithub')
+    const nada = document.getElementById('tudo')
+    txt.style.display = 'none';
+    nada.style.display = 'block';
+}
+
+function encima6() {
+    const txt = document.getElementById('textojs')
+    const nada = document.getElementById('tudo')
+    txt.style.display = 'block';
+    nada.style.display = 'none';
+}
+
+function fora6() {
+    const txt = document.getElementById('textojs')
+    const nada = document.getElementById('tudo')
+    txt.style.display = 'none';
+    nada.style.display = 'block';
+}
+
+function encima7() {
+    const txt = document.getElementById('textocss')
+    const nada = document.getElementById('tudo')
+    txt.style.display = 'block';
+    nada.style.display = 'none';
+}
+
+function fora7() {
+    const txt = document.getElementById('textocss')
+    const nada = document.getElementById('tudo')
+    txt.style.display = 'none';
+    nada.style.display = 'block';
+}
+
+function encima8() {
+    const txt = document.getElementById('textohtml')
+    const nada = document.getElementById('tudo')
+    txt.style.display = 'block';
+    nada.style.display = 'none';
+}
+
+function fora8() {
+    const txt = document.getElementById('textohtml')
+    const nada = document.getElementById('tudo')
+    txt.style.display = 'none';
+    nada.style.display = 'block';
+}
